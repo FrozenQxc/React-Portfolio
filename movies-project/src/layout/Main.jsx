8111c21981673fa315e0fa2ef3311af8ed098a5c
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-04-20 17:52:14 +0300

    version 1.1

--- movies-project/src/layout/Main.jsx
+++ movies-project/src/layout/Main.jsx
@@ -1,7 +1,31 @@
-function Main() {
-    return <main className="container content">
-        Hello from Movies app
-    </main>
+import React from "react";
+import {Movies} from '../components/Movies';
+
+
+class Main  extends React.Component { 
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=69f516d8&s=Bleach')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+    
+        render() {
+            const {movies} = this.state;
+
+            return <main className="container content">
+                {
+                    movies.length ?  (
+                    <Movies movies={this.state.movies} />
+                    ) : <h3>Загрузка...</h3>
+                }
+
+            </main>
+        }
+
 }
 
 export {Main}
