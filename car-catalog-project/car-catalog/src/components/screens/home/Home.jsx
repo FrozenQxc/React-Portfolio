eaaf3d39d4fe92609057af5c37444025e8d61ed8
timofey stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-04-24 19:50:00 +0300

    create form

--- car-catalog-project/car-catalog/src/components/screens/home/Home.jsx
+++ car-catalog-project/car-catalog/src/components/screens/home/Home.jsx
@@ -1,27 +1,22 @@
-import styles from './Home.module.css'
-import { cars } from './cars.data.js'
+import { cars as carsData } from './cars.data.js';
+import CarItem from './car-item/CarItem.jsx';
+import CreateCarForm from './create-car-form/CreateCarForm.jsx';
+import { useState } from 'react';
 
 function Home() {
+  const [cars, setCars] = useState(carsData)
+  
   return (
     <div>
+      <CreateCarForm setCars={setCars} />
       <h1>Cars catalog</h1>
       <div>
-        {cars.map(car => (
-          <div  key= {car.id} className={styles.item}>
-            <div className={styles.image}
-              style={{ backgroundImage: `url(${car.image})` }}>
-            </div>
-            <div className={styles.info}>
-              <h2>{car.name}</h2>
-              <p>{car.price}</p>
-              <button>Read more</button>
-            </div>
-          </div>
-        ))}
-
+        {cars.length ? cars.map(car =>
+          (<CarItem key={car.id} car={car} />))
+          :<p>Машин нет</p>}
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
