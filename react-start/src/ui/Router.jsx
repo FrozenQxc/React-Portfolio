4b229a058d49f2f4534e5ba1a7646051e75b86c9
Timofey Stepanov <125030768+FrozenQxc@users.noreply.github.com>
2023-07-14 22:12:34 +0300

    fix

--- react-start/src/ui/Router.jsx
+++ react-start/src/ui/Router.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../home/Home';
 import Hook from '../pages/hook/Hook';
 import Main from '../pages/work/Main';
-import CarsMain from '../pages/CarsCatalog/CarsMain';
+
 
 
 const Router = () => {
@@ -13,7 +13,7 @@ const Router = () => {
         <Route  element= {<Home />} path='/' />
         <Route element= {<Hook />} path ='/hook' />
         <Route element= {<Main />} path ='/1' />
-        <Route element= {<CarsMain />} path ='/2' />
+
     
         <Route path='*' element = {<div>Страница не найдена</div>}  />
       </Routes>
